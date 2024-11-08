27b9f9ed4169af9d32cae31b8e498bf5db992b6c
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-11-08 18:04:38 +0300

    hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task1 = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task1 = true;
+    let task = true;
     let task2 = false;
   }
 
-  return [task1, task2];
+  return [task, task2];
 }
