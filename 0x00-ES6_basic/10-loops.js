0af47f0bd9ef146dafc0f531afdf648069e1f663
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-11-09 11:29:28 +0300

    debugging

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+    for (let i = 0; i < array.length; i++) {
+      array[i] += appendString;
+    }
+  
+    return array;
   }
-
-  return array;
-}
+  
