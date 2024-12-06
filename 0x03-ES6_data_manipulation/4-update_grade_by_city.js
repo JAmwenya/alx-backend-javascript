3c4837868fb0aa402d6c13a1bce7cd6ed429055a
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-12-06 10:42:03 +0300

    data manipulation with javascript

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    return students
+      .filter(student => student.location === city)
+      .map(student => {
+        const grade = newGrades.find(g => g.studentId === student.id)?.grade || 'N/A';
+        return { ...student, grade };
+      });
+  }
+  
