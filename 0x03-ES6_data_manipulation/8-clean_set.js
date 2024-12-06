7bfbb27a7ddfb34881db05cb0396e81430bdc500
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-12-06 12:42:48 +0300

    debugging linters

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,8 +3,8 @@ export default function cleanSet(set, startString) {
       return '';
     }
     return [...set]
-      .filter(value => typeof value === 'string' && value.startsWith(startString))
-      .map(value => value.slice(startString.length))
+      .filter((value) => typeof value === 'string' && value.startsWith(startString))
+      .map((value) => value.slice(startString.length))
       .join('-');
   }
   
