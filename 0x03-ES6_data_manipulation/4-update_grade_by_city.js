83110fa825f0214d1c8ac4b9e48b03491110ed92
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-12-09 13:45:46 +0300

    debugging linters

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const grade = newGrades.find((g) => g.studentId === student.id)?.grade || 'N/A';
-      return { ...student, grade };
-    });
+return students
+  .filter((student) => student.location === city)
+  .map((student) => {
+    const grade = newGrades.find((g) => g.studentId === student.id)?.grade || 'N/A';
+    return { ...student, grade };
+  });
 }
