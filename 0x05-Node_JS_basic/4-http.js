7ba0456cf388584f781763d66799f991c26d93c9
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-12-11 21:51:48 +0300

    debugging 1245 server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,23 +1,12 @@
 const http = require('http');
 
-
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
-  } else if (req.url === '/test') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Test Endpoint\n');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
-  }
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
-
 
 app.listen(1245, () => {
-  console.log('Server running at http://localhost:1245/');
+  console.log('Server is running on port 1245');
 });
-
 
 module.exports = app;
