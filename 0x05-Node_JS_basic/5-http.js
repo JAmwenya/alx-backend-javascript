f34d144ab2d3360d28ae7963670facfd4f45b0f1
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-12-11 21:54:57 +0300

    changing ALX to Holberton

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,7 +5,7 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello ALX!');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
