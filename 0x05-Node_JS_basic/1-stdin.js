7fd605ff5ae4b380e6cc778c3c73b891670b3cac
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-12-11 18:09:43 +0300

    debugging linters

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,9 +8,7 @@ const rl = readline.createInterface({
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  // Handle the trailing carriage return for compatibility with the test
-  const trimmedInput = input.replace(/\r$/, '');
-  console.log(`Your name is: ${trimmedInput}\r`);
+  console.log(`Your name is: ${input}`);
   rl.close();
 });
 
