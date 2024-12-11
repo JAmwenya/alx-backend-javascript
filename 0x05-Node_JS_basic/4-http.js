008b1fbcd58525228a1be24372afb18b87c38901
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-12-11 18:37:01 +0300

    debugging server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,23 @@
 const http = require('http');
 
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello ALX!');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello ALX!\n');
+  } else if (req.url === '/test') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Test Endpoint\n');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
 });
 
+
 app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+  console.log('Server running at http://localhost:1245/');
 });
+
 
 module.exports = app;
