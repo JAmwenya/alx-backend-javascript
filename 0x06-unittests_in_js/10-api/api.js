00debf7d85a503dd201c92c58837c125a56c3901
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-12 16:23:43 +0300

    Update api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -10,6 +10,15 @@ app.get("/available_payments", (req, res) => {
 	res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
+app.get("/cart/:id", (req, res) => {
+  const { id } = req.params;
+  // Ensure that id is a valid number
+  if (isNaN(id)) {
+    return res.status(400).send("id must be a number");
+  }
+  res.send(`Payment methods for cart ${id}`);
+});
+
 app.post("/login", (req, res) => {
 	const { userName } = req.body;
 	res.send(`Welcome ${userName}`);
