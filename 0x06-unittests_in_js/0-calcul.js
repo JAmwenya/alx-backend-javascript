95c3de33b85d4897de0ef2c1cd4a24f576d9c57a
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-12 17:12:19 +0300

    Update 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,5 @@
 function calculateNumber(a, b) {
-	return Math.round(a) + Math.round(b);
+    return Math.ceil(a) + Math.round(b);  // Round first number with Math.ceil() and second with Math.round()
 }
 
 module.exports = calculateNumber;
