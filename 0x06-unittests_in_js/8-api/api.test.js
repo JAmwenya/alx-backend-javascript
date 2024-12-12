3975d3399b62928e89cb7cf7d4f08b14164d31e7
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-12 20:32:39 +0300

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,57 +1,28 @@
-// 8-api/api.test.js
+// api.test.js
 
-const request = require("request");
 const { expect } = require("chai");
-const http = require("http");
+const supertest = require("supertest");
+const request = supertest("http://localhost:7865");
 
-describe("Index page", () => {
-	let server;
-
-	// Start the server before running the tests
-	before((done) => {
-		// Import the express app from api.js and start the server
-		server = require("./api.js");
-		done();
-	});
-
-	// Close the server after the tests complete
-	after((done) => {
-		if (server) {
-			server.close(() => {
-				done(); 
-			});
-		} else {
-			done();
-		}
-	});
-
-	it("should return status code 200", (done) => {
-		request("http://localhost:7865", (error, response, body) => {
-			expect(response.statusCode).to.equal(200); 
+describe("Index page", function () {
+	it("should return a status code of 200", function (done) {
+		request.get("/").end((err, res) => {
+			expect(res.status).to.equal(200);
 			done();
 		});
 	});
 
-	it('should return "Welcome to the payment system"', (done) => {
-		request("http://localhost:7865", (error, response, body) => {
-			expect(body).to.equal("Welcome to the payment system");
+	it("should return correct message", function (done) {
+		request.get("/").end((err, res) => {
+			expect(res.text).to.equal("Welcome to the payment system");
 			done();
 		});
 	});
-});
 
-describe("GET /test", () => {
-	it("should return Welcome to the payment system", async () => {
-		const response = await request(app).get("/test");
-		expect(response.status).toBe(200);
-		expect(response.text).toBe("Welcome to the payment system");
-	});
-});
-
-describe("GET /", () => {
-	it("should return Hello", async () => {
-		const response = await request(app).get("/");
-		expect(response.status).toBe(200);
-		expect(response.text).toBe("Hello");
+	it("should return a content-type header of text/html", function (done) {
+		request.get("/").end((err, res) => {
+			expect(res.headers["content-type"]).to.include("text/html");
+			done();
+		});
 	});
 });
