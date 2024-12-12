e918d8382e648fefcd08445a00fc0c09ee3381a4
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-12 17:48:11 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,18 @@
 // 0-calcul.test.js
 
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
 
-describe("Test suite for calculateNumber function", function () {
+describe("calculateNumber", function () {
+	it("should round and add 1.4 and 4.5", function () {
+		assert.strictEqual(calculateNumber(1.4, 4.5), 6);
+	});
 
-  // Test for first number rounded using Math.ceil()
-  it("should round and add the first number using Math.ceil()", function () {
-    assert.strictEqual(calculateNumber(1.4, 4.5), 7);
-    assert.strictEqual(calculateNumber(2.1, 3.3), 6);
-    assert.strictEqual(calculateNumber(0.7, 2.2), 3);
-  });
+	it("should round and add 1.2 and 3.7", function () {
+		assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+	});
 
-  // Test for second number rounded using Math.round()
-  it("should round and add the second number using Math.round()", function () {
-    assert.strictEqual(calculateNumber(1.4, 4.5), 7);
-    assert.strictEqual(calculateNumber(2.1, 3.7), 7);
-    assert.strictEqual(calculateNumber(5.4, 2.2), 8);
-  });
-
-  // Test for both numbers rounded
-  it("should round and add both numbers", function () {
-    assert.strictEqual(calculateNumber(1.4, 4.5), 7);
-    assert.strictEqual(calculateNumber(2.1, 3.7), 7);
-    assert.strictEqual(calculateNumber(5.5, 2.2), 8);
-  });
+	it("should round and add 1.5 and 3.7", function () {
+		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+	});
 });
