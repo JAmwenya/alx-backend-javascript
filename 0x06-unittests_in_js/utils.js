a432b11e755f64d65de2584047e8c9e04f837634
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-12 18:41:25 +0300

    Update utils.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,2 +1,22 @@
-const calculateNumber = require("./1-calcul");
-module.exports = { calculateNumber };
+// utils.js
+
+const Utils = {
+	calculateNumber(type, a, b) {
+		const roundedA = Math.round(a);
+		const roundedB = Math.round(b);
+
+		if (type === "SUM") {
+			return roundedA + roundedB;
+		} else if (type === "SUBTRACT") {
+			return roundedA - roundedB;
+		} else if (type === "DIVIDE") {
+			if (roundedB === 0) {
+				return "Error";
+			} else {
+				return Math.round((roundedA / roundedB) * 10) / 10;
+			}
+		}
+	},
+};
+
+module.exports = Utils;
